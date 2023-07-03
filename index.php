0ad489047e69308268781a930cb85d3576701cd7
simonescoca <simone.scoca@icloud.com>
2023-07-03 13:37:51 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!-- Descrizione:
+    Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G
+    Stampare tutti i nostri hotel con tutti i dati disponibili, iniziate in modo graduale.
+    Prima stampate in pagina i dati, senza preoccuparvi dello stile.
+    Dopo aggiungete Bootstrap e mostrate le informazioni con una tabella.
+
+Bonus:
+    1 - Aggiungere un form ad inizio pagina che tramite una richiesta GET permetta di filtrare gli hotel che hanno un parcheggio.
+    2 - Aggiungere un secondo campo al form che permetta di filtrare gli hotel per voto
+    (es. inserisco 3 ed ottengo tutti gli hotel che hanno un voto di tre stelle o superiore)
+
+NOTA:
+    deve essere possibile utilizzare entrambi i filtri contemporaneamente
+    (es. ottenere una lista con hotel che dispongono di parcheggio e che hanno un voto di tre stelle o superiore)
+    Se non viene specificato nessun filtro, visualizzare come in precedenza tutti gli hotel. -->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hotels</title>
+    </head>
+    <body>
+        <?php
+            $hotels = [
+                [
+                    'name' => 'Hotel Belvedere',
+                    'description' => 'Hotel Belvedere Descrizione',
+                    'parking' => true,
+                    'vote' => 4,
+                    'distance_to_center' => 10.4
+                ],
+                [
+                    'name' => 'Hotel Futuro',
+                    'description' => 'Hotel Futuro Descrizione',
+                    'parking' => true,
+                    'vote' => 2,
+                    'distance_to_center' => 2
+                ],
+                [
+                    'name' => 'Hotel Rivamare',
+                    'description' => 'Hotel Rivamare Descrizione',
+                    'parking' => false,
+                    'vote' => 1,
+                    'distance_to_center' => 1
+                ],
+                [
+                    'name' => 'Hotel Bellavista',
+                    'description' => 'Hotel Bellavista Descrizione',
+                    'parking' => false,
+                    'vote' => 5,
+                    'distance_to_center' => 5.5
+                ],
+                [
+                    'name' => 'Hotel Milano',
+                    'description' => 'Hotel Milano Descrizione',
+                    'parking' => true,
+                    'vote' => 2,
+                    'distance_to_center' => 50
+                ]
+            ];
+        ?>
+    </body>
+</html>
